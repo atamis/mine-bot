cddcdbe790de46ab28f602815fdaad03918f0fb8
Andrew Amis <atamiser@gmail.com>
2019-02-02 13:00:56 -0800

    Added Discord bot code.
    Includes bot code, fixes to minesweeper code, and upgrade to Clojure
    1.10.0.

--- src/mine_bot/core.clj
+++ src/mine_bot/core.clj
@@ -1,7 +1,45 @@
 (ns mine-bot.core
+  (:require [discord.bot :as bot]
+            [discord.http :as http]
+            [mine-bot.minesweeper :as minesweeper])
   (:gen-class))
 
+(comment
+  :say
+  (fn [client message]
+    (println "Recieved" message)
+    (comment (:content
+              message))
+    (bot/say (:content message))))
+
+(defn respond-mention
+  [message]
+  (str "<" (:mention (:author message)) ">")
+  )
+
+(defn minesweeper-extension
+  [client message]
+  (let [[x y mines] (->> (clojure.string/split (:content message) #" ")
+                         (map #(Integer/parseInt %)))
+        x (or x 10)
+        y (or y 10)
+        mines (or mines 10)
+        string (minesweeper/minesweeper-render x y mines)
+        ]
+    (cond
+
+      (< 2000 (count string)) (bot/say (str (respond-mention message) ", board too large."))
+      (< 197 (* x y)) (bot/say (str (respond-mention message) ", maximum 197 spoilers per post, board too large."))
+      :else (bot/say (minesweeper/minesweeper-render x y mines))
+      )
+    ))
+
 (defn -main
-  "I don't do a whole lot ... yet."
+  "Discord bot to generate minesweeper boards."
   [& args]
-  (println "Hello, World!"))
+  (println "Launching...")
+  (bot/with-extensions
+    "MinesweeperBot" "^"
+    :hello (fn [client message] (println "Received" message)
+             (bot/say (str "Hello, <" (:mention (:author message)) ">")))
+    :mine minesweeper-extension))
